4bbff5b9c01eeffdf65fd6d5bde672645f367497
Dean Howe <hideanhowe@gmail.com>
2023-12-15 23:40:49 +0000

    javascript functionality

---
+++ script.js
@@ -0,0 +1,81 @@
+const form = document.querySelector('.rating__form');
+const numbers = document.querySelectorAll('.list__item');
+const ratingCard = document.querySelector('.rating__card');
+const thanksCard = document.querySelector('.thanks__card');
+const resetBtn = document.querySelector('.reset__btn');
+const formList = document.querySelector('.form__list');
+let score = 0;
+
+// clear state
+function clearState() {
+  formListRemoveRing();
+  numbers.forEach((item) => {
+    if (
+      item.classList.contains('bg-l-gray') &&
+      item.classList.contains('text-white')
+    ) {
+      item.classList.remove('bg-l-gray');
+      item.classList.add('bg-d-gray');
+      item.classList.remove('text-white');
+    }
+  });
+}
+
+// Toggle classes & set score
+function toggle(e) {
+  clearState();
+  e.target.classList.toggle('bg-l-gray');
+  e.target.classList.toggle('bg-d-gray');
+  e.target.classList.toggle('text-white');
+
+  score = e.target.getAttribute('data-set');
+  //   console.log(score);
+}
+
+// openThanks
+function openThanks() {
+  ratingCard.classList.toggle('hidden');
+  thanksCard.classList.toggle('hidden');
+}
+
+// formList Add Ring
+function formListAddRing() {
+  formList.classList.add('ring');
+}
+// formList Remove Ring
+function formListRemoveRing() {
+  formList.classList.remove('ring');
+}
+
+// Form onSubmit
+function onSubmit(e) {
+  e.preventDefault();
+  if (score > 0) {
+    console.log(score);
+    openThanks();
+  } else if (score === 0) {
+    formListAddRing();
+  }
+}
+
+// Reset to RatingCard
+function resetRating() {
+  openThanks();
+  clearState();
+  score = 0;
+}
+
+// Form submission addEventListener with score
+form.addEventListener('submit', onSubmit);
+
+// AddEventListener
+
+numbers.forEach((item) => {
+  item.addEventListener('click', function (listItem) {
+    toggle(listItem);
+  });
+});
+
+// AddEventListener resetBtn
+
+resetBtn.addEventListener('click', resetRating);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Overpass'],
+    },
+    extend: {
+      colors: {
+        'fm-orange': 'hsl(25, 97%, 53%)',
+        'l-gray': 'hsl(217, 12%, 63%)',
+        'md-gray': 'hsl(216, 12%, 54%)',
+        'd-gray': 'hsl(213, 19%, 18%)',
+        'v-dark': 'hsl(216, 12%, 8%)',
+      },
+      // fontSize: {
+      //   'leading-tiny': '',
+      // },
+    },
   },
   plugins: [],
 };
